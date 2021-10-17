91d36f4287ec04c8fb3a969a3c653c209693f504
GayathmiKariyapperuma <gayathmikariyapperuma@gmail.com>
2021-10-17 13:15:03 +0530

    re-positioned lines

--- youtube_download.py
+++ youtube_download.py
@@ -7,7 +7,7 @@ root.resizable(0,0)
 root.title("Youtube Video Downloader")
 
 
-Label(root,text = 'Youtube Video Downloader', font ='Ubuntu 20 bold').pack()
+Label(root,text = '\n Youtube Video Downloader', font ='Ubuntu 20 bold').pack()
 
 
 
@@ -15,8 +15,8 @@ Label(root,text = 'Youtube Video Downloader', font ='Ubuntu 20 bold').pack()
 ##enter link
 link = StringVar()
 
-Label(root, text = 'Paste Link Here:', font = 'Ubuntu 15 ').place(x= 160 , y = 60)
-link_enter = Entry(root, width = 70,textvariable = link).place(x = 32, y = 90)
+Label(root, text = 'Paste Link Here:', font = 'Ubuntu 15 ').place(x= 160 , y = 85)
+link_enter = Entry(root, width = 70,textvariable = link).place(x = 32, y = 130)
 
 
 
@@ -31,7 +31,7 @@ def Downloader():
     Label(root, text = 'DOWNLOADED', font = 'Roboto 15').place(x= 180 , y = 210)  
 
 
-Button(root,text = 'DOWNLOAD', font = 'Ubuntu 15 bold' ,bg = 'light blue', padx = 2, command = Downloader).place(x=180 ,y = 150)
+Button(root,text = 'DOWNLOAD', font = 'Ubuntu 15 bold' ,bg = 'light blue', padx = 2, command = Downloader).place(x=180 ,y = 170)
 
 
 
